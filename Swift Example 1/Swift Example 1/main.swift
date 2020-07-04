dd449ca641778450776321a0bcf7ace86b95a9fa
Kai Kluge <kluge@candy4you.de>
2020-07-04 13:04:12 +0200

    Created several variables, printed values and wrote for loop.

---
+++ Swift Example 1/Swift Example 1/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Swift Example 1
+//
+//  Created by Kai Kluge on 04.07.20.
+//  Copyright © 2020 Kai Kluge. All rights reserved.
+//
+
+import Foundation
+
+var name: String = "Kai"
+var age: Int = 54
+var value: Double = 154321
+
+print("Name is \(name)")
+print("\(name) is \(age) years old")
+
+for i in 1...5 {
+    print("i = \(i)")
+}
+
